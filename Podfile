0b4f1a47df9437715a59488f6eb8eb1a9e441a6a
sashablokhin <sashablokhindev@gmail.com>
2016-02-18 17:29:35 +0300

    Added firebase, facebook and googlemaps frameworks

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'FirebaseLocation' do
+    pod 'Firebase', '>= 2.5.0'
+end
+
---
+++ Pods/Target Support Files/Pods-FirebaseLocation/Pods-FirebaseLocation-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_FirebaseLocationVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FirebaseLocationVersionString[];
+
